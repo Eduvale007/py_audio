fab826ea435cf9c85255040283e1eda79e0811be
Eduvale007 <eduvale007@gmail.com>
2025-10-08 16:47:28 -0300

    new commit

---
+++ 04-gtts.py
@@ -0,0 +1,25 @@
+from gtts import gTTS
+from playsound import playsound
+import os
+
+def cria_audio(mensagem):
+    tts = gTTS(mensagem, lang='pt-br')
+    tts.save('dados/mensagem.mp3')
+    playsound('dados/mensagem.mp3')
+    os.remove('dados/mensagem.mp3')
+
+
+ #1-usando diretamente
+cria_audio('Aprendendo a criar um assistente virtual') 
+
+
+ #2-usando com input
+frase = input('Digite a frase a ser falada:\n')
+cria_audio(frase)
+
+
+#3-usando com arquivo
+
+arquivo = open("dados/frase.txt", 'r', encoding='utf-8')
+conteudo = arquivo.read()
+cria_audio(conteudo)
